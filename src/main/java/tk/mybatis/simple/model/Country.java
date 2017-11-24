0d4a05a77bc3cea78116e7f67758b5854b7a718a
Administrator <Administrator@class3>
2017-11-24 12:28:59 +0800

    mybatis基础环境的搭建测试

---
+++ src/main/java/tk/mybatis/simple/model/Country.java
@@ -0,0 +1,48 @@
+package tk.mybatis.simple.model;
+
+public class Country {
+	private Long id;
+	private String countryname;
+	private String countrycode;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCountryname() {
+		return countryname;
+	}
+
+	public void setCountryname(String countryname) {
+		this.countryname = countryname;
+	}
+
+	public String getCountrycode() {
+		return countrycode;
+	}
+
+	public void setCountrycode(String countrycode) {
+		this.countrycode = countrycode;
+	}
+
+	
+	public Country() {
+	}
+	public Country(Long id, String countryname, String countrycode) {
+		this.id = id;
+		this.countryname = countryname;
+		this.countrycode = countrycode;
+	}
+
+	@Override
+	public String toString() {
+		return "Country [id=" + id + ", countryname=" + countryname + ", countrycode=" + countrycode + "]";
+	}
+
+	
+	
+}
